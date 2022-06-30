7dbaa1b4a475d978077c52d58d863a4955c66054
Gaurav Yadav <gaurav.yadav@calidig.com>
2022-06-30 22:00:23 +0530

    Initial Commit

---
+++ lib/screens/video_tile.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class VideoTile extends StatefulWidget {
+  const VideoTile({Key? key}) : super(key: key);
+
+  @override
+  State<VideoTile> createState() => _VideoTileState();
+}
+
+class _VideoTileState extends State<VideoTile> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
